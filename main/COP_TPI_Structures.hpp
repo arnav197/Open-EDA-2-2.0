00b7ee79d63b33e1160b3a4cc74542ea2ecabc36
Arnav <arnav197@gmail.com>
2022-02-23 14:59:44 +0530

    indent issues fix

--- main/COP_TPI_Structures.hpp
+++ main/COP_TPI_Structures.hpp
@@ -1,4 +1,4 @@
-/**
+/*
  * @file FaultStructures.hpp
  * @author Spencer Millican (millican@auburn.edu)
  * @version 0.1
@@ -20,6 +20,7 @@
 template <class _valueType>
 class Faulty;
 
+
 /*
  * A line which can be used for cop-based TPI and fault simulation.
  *
@@ -30,6 +31,7 @@ class Faulty;
 template <class _width>
 class COP_TPI_Line : virtual public COPLine<_width>, virtual public FaultyLine<_width> {
 public:
+	
 	/*
 	 * Create a with a "UNDEF" name.
 	 */
@@ -37,11 +39,13 @@ public:
 		_flag = false;
 	};
 
+	
 	/*
 	 * Create a Line with a given name
 	 *
 	 * @param _name The name of the Line
 	 */
+	
 	COP_TPI_Line(std::string _name) :
 		COPLine<_width>(_name),
 		FaultyLine<_width>(_name),
@@ -51,11 +55,14 @@ public:
 		_flag = false;
 	};
 
+
 	virtual Connecting* clone() const {
 
 		return new COP_TPI_Line<_width>(this->name());
 	};
 
+
+	
 	void setflag(bool flag)
 	{
 		_flag = flag;
@@ -65,9 +72,12 @@ public:
 		return _flag;
 	}
 
+
 private:
 	bool _flag = false;
 };
+
+
 
 /*
  * A node which can accept FaultyLines as inputs and outputs.
@@ -79,9 +89,11 @@ private:
 template <class _width>
 class COP_TPI_Node : public virtual COPNode<_width>, virtual public FaultyNode<_width> {
 public:
+	
 	/*
 	 * Create a node with no inputs, no outputs, and a "copy" function.
 	 */
+
 	COP_TPI_Node() : COPNode<_width>(), FaultyNode<_width>(), Connecting() {
 		_flag = false;
 
@@ -91,11 +103,15 @@ public:
 	 * Create a Boolean node.
 	 *
 	 * @param _function The function of this node (nullptr = "copy node")
+	 * 
 	 * @param (optional) _name The name of the node.
+	 * 
 	 * @param (optional) _inputs Input lines to the given node.
+	 * 
 	 * @param (optional) _outputs Output lines to the given node.
 	 */
 	COP_TPI_Node(
+		
 		Function<_width>* _function,
 		std::string _name,
 		std::unordered_set<COP_TPI_Line<_width>*> _inputs = std::unordered_set<COP_TPI_Line<_width>*>(),
@@ -109,32 +125,43 @@ public:
 			std::unordered_set<COPLine<_width>*>(_inputs.begin(), _inputs.end()),
 			std::unordered_set<COPLine<_width>*>(_outputs.begin(), _outputs.end())
 			),
+		
+		
 		FaultyNode<_width>(
 			_function,
 			_name,
 			std::unordered_set<FaultyLine<_width>*>(_inputs.begin(), _inputs.end()),
 			std::unordered_set<FaultyLine<_width>*>(_outputs.begin(), _outputs.end())
 			),
+		
+		
 		Valued<_width>(
 			_function
 			),
+		
+		
 		Connecting(
 			std::unordered_set<Connecting*>(_inputs.begin(), _inputs.end()),
 			std::unordered_set<Connecting*>(_outputs.begin(), _outputs.end()),
 			_name
 		)
+	
+	
 	{
 		_flag = false;
 	};
 
+	
 	void setflag(bool flag)
 	{
 		_flag = flag;
 	};
 
+	
 	bool getflag() {
 		return _flag;
 	}
+
 
 private:
 	bool _flag = false;
@@ -142,5 +169,5 @@ private:
 
 
 
-#endif //COP_TPI_Structures_h
+#endif   //COP_TPI_Structures_h
 
